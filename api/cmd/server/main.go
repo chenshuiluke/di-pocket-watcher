3cf7d9e4ba23fc18d2003d0a300438f5ae37e37d
Luke Chen Shui <chenshuiluke@gmail.com>
2024-10-05 18:24:32 -0500

    Made some updates

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -1,12 +1,38 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/chenshuiluke/di-pocket-watcher/api/internal/db"
+	"github.com/gofiber/fiber/v2"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 func main() {
+	ctx := context.Background()
+	url := fmt.Sprintf("postgres://%s:%s@%s:5432/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+
+	conn, err := pgxpool.New(ctx, url)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
+	defer conn.Close()
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		queries := db.New(conn)
+		results, err := queries.ListUsers(ctx)
+		if err != nil {
+			log.Println(err)
+		}
+		return c.JSON(results)
 	})
 
 	app.Listen(":8080")
